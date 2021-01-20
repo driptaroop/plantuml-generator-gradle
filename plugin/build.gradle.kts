72bd0c7010f2b7ca97e9b995da19530bacb212d0
Driptaroop Das <driptaroop.das@dkbcodefactory.com>
2021-01-20 19:23:18 +0100

    updated gradle to 6.8

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -3,12 +3,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     `java-gradle-plugin`
     id("com.gradle.plugin-publish") version "0.12.0"
-    kotlin("jvm") version "1.3.72"
-    id("org.jmailen.kotlinter") version "2.4.1"
+    kotlin("jvm") version "1.4.21"
+    id("org.jmailen.kotlinter") version "3.3.0"
     maven
 }
 group = "org.dripto.gradle.plugin.plantuml"
-version = "0.0.1"
+version = "0.0.2"
 description = "Generate plantUml diagram from code using gradle"
 val pluginId = "org.dripto.gradle.plugin.plantuml.plantuml-generator"
 val githubUrl ="https://github.com/driptaroop/plantuml-generator-gradle"
